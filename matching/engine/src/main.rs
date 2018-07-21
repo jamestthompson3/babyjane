27603b9a059539bf1881f8d4a1e7664ee1746c73
jamestthompson3 <jamestthompson3@gmail.com>
2018-07-21 18:09:42 +0300

    [Matching] Adds basic logical operator for messages

--- matching/engine/src/main.rs
+++ matching/engine/src/main.rs
@@ -1,41 +1,28 @@
-extern crate ws;
 extern crate env_logger;
+extern crate ws;
 
-use ws::{connect, CloseCode};
+use ws::connect;
+use ws::Message;
 
-
-fn main () {
-
+fn main() {
     // Setup logging
     env_logger::init();
 
     // Connect to the url and call the closure
-    if let Err(error) = connect("ws://127.0.0.1:8000", |out| {
-
-        // Queue a message to be sent when the WebSocket is open
-        if let Err(_) = out.send("Hello WebSocket") {
-            println!("Websocket couldn't queue an initial message.")
-        } else {
-            println!("Client sent message 'Hello WebSocket'. ")
-        }
-
+    connect("ws://127.0.0.1:8000", |out| {
         // The handler needs to take ownership of out, so we use move
-        move |msg| {
-            // match msg {
-            // Messages::Buy => println!("Client got buy message '{}'. ", msg),
-            // Messages::Sell => println!("Client got sell message '{}'. ", msg)
-            // }
-
-            // Handle messages received on this connection
-           println!("Client got message '{}'. ", msg);
-
-            // Close the connection
-            out.close(CloseCode::Normal)
+        move |msg: Message| {
+            let is_text = msg.is_text();
+            println!("Classification of msg {}", is_text);
+            if is_text {
+                println!("message receieved as text, {}", msg);
+            } else {
+                println!("message receieved, {}", msg);
+                out.send("Invalid data format").unwrap();
+            }
+            Ok(())
         }
-
-    }) {
-        // Inform the user of failure
-        println!("Failed to create WebSocket due to: {:?}", error);
-    }
-
+        // Close the connection
+        // out.close(CloseCode::Normal)
+    }).unwrap();
 }
