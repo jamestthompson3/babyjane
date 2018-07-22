273370a47e2af4ff18f63546112e28406637da4c
jamestthompson3 <jamestthompson3@gmail.com>
2018-07-22 21:08:57 +0300

    [Matching] Appends array of incoming requests, need to fix matching

--- matching/engine/src/lib.rs
+++ matching/engine/src/lib.rs
@@ -5,21 +5,35 @@ extern crate serde_json;
 
 use serde_json::Error;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Copy, Clone)]
 pub struct RequestAction {
-    class: u8,
-    price: usize,
-    quantity: usize,
+    action: u32,
+    price: f32,
+    quantity: u32,
+    id: u32,
 }
 
+pub struct Match((u32, f32, u32), (u32, f32, u32));
+
 pub fn decode_text(text: &str) -> Result<(), Error> {
-    println!("{}", text);
-    // FIXME
-    let p: RequestAction = serde_json::from_str(text)?;
-    println!("made it past request action creation");
-    // println!(
-    //     "text decoded {} requested at {} price and {} quantity",
-    //     p.class, p.price, p.quantity
-    // );
+    let mut candidates: Vec<RequestAction> = Vec::new();
+    let mut matches: Vec<Match> = Vec::new();
+    let incoming_request: RequestAction = serde_json::from_str(text)?;
+    candidates.push(incoming_request);
+    // FIXME  WORKING UP TO HERE
+    for candidate in candidates {
+        println!("candidate {}", candidate.id);
+        if candidate.action == incoming_request.action {
+            matches.push(Match(
+                (candidate.id, candidate.price, candidate.quantity),
+                (
+                    incoming_request.id,
+                    incoming_request.price,
+                    incoming_request.quantity,
+                ),
+            ));
+        }
+    }
+    println!("Action complete");
     Ok(())
 }
